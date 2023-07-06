c426542a64ca4617fdf4e3585748e956ae0242ac
aymaneharkati <aymaneharkati@gmail.com>
2023-07-06 14:37:16 +0100

    finilazed the image generator

--- Image-Gen/main.js
+++ Image-Gen/main.js
@@ -1,22 +1,42 @@
-import './style.css'
+import "./style.css";
 
-const form = document.querySelector('form');
-form.addEventListener('submit',async (e) => {
-e.preventDefault();
-const data = new FormData(form);
-console.log(data.get('prompt'));
-const response = await fetch("http://localhost:8080/dream",{
-  method : 'POST',
-  headers : {
-    'Content-Type': 'application/json',
-  },
-  
-  body : JSON.stringify({
-    prompt: data.get('prompt'),
-  })
-})
-const {image} = await response.json();
-const results = document.querySelector('#result');
-console.log(image[0])
-results.innerHTML = `<img src="${image[0]}"/>`;
+const form = document.querySelector("form");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  showSpinner();
+  const data = new FormData(form);
+  const response = await fetch("http://localhost:8080/dream", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  }).catch(() => {
+    alert("Server is down....try later");
+    hideSpinner();
+  });
+  if (response.ok) {
+    const { image } = await response.json();
+    const results = document.querySelector("#result");
+    results.innerHTML = `<img src="${image[0]}"/>`;
+  } else {
+    const err = await response.text();
+    alert(err);
+  }
+
+  hideSpinner();
 });
+
+function showSpinner() {
+  const button = document.querySelector("button");
+  button.disabled = true;
+  button.innerHTML = 'Dreaming... <span class="spinner">🧠</span>';
+}
+function hideSpinner() {
+  const button = document.querySelector("button");
+  button.disabled = false;
+  button.innerHTML = "Dream";
+}
--- Image-Gen/server.js
+++ Image-Gen/server.js
@@ -1,6 +1,5 @@
 import * as dotenv from 'dotenv';
 dotenv.config()
-const openai = new OpenAIApi(config);
 import  express from 'express';
 import cors from 'cors';
 import axios from 'axios';
@@ -9,12 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/dream', async (req, res) => {
-  const prompt = req.body.prompt;
+  try{const prompt = req.body.prompt;
   const key = process.env.API_KEY
   const aiResp = await axios.post('https://stablediffusionapi.com/api/v3/text2img', {"key":key,"prompt":prompt,"width": "512",
   "height": "512"})
 const image = aiResp.data.output;
-res.send({image});
+res.send({image});}
+catch(err){
+  console.error(err);
+  res.status(500).send('Something went wrong');
+}
 });
 
 app.listen(8080, () => console.log('Server Started at http://localhost:8080/dream'))
